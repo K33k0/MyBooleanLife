2534282541f3753d5580b7f1bd58245498502947
Kieran Wynne <keewynne90@gmail.com>
2023-05-08 22:08:57 +0100

    Implements fastapi. Adds some paths to get started

--- MyBooleanLife/db.py
+++ MyBooleanLife/db.py
@@ -22,15 +22,27 @@ __SQL_TASK_COMPLETION_TABLE_CREATE = """
                         );"""
 __SQL_INIT_COMPLETION_DATE_TABLE = """INSERT INTO Completion_Date (Date) VALUES (?)"""
 __SQL_ADD_NEW_TASK = """INSERT INTO Tasks (Name, Date_Added) VALUES (?,?)"""
-__SQL_READ_TASK_BY_NAME = """SELECT ID from Tasks WHERE Name = (?)"""
+__SQL_READ_TASK_ID_BY_NAME = """SELECT ID from Tasks WHERE Name LIKE (?)"""
 __SQL_LOG_COMPLETION = """INSERT INTO Task_Completion (Task_ID, Completion_Date_ID) VALUES (
                         (SELECT ID FROM TASKS WHERE Name = ?),
                         (SELECT ID FROM Completion_Date WHERE Date = ?)
                         )"""
-
+__SQL_READ_TASK_NAMES = "SELECT Name from Tasks"
+__SQL_READ_TASK_COMPLETIONS_BY_NAME = """SELECT Completion_Date.Date FROM Task_Completion
+                                 JOIN Tasks ON Task_ID = Tasks.ID
+                                 JOIN Completion_Date ON Completion_Date_ID = Completion_Date.ID
+                                 WHERE Tasks.Name = (?)"""
+__SQL_READ_TASK_COMPLETIONS_BY_NAME_AND_DATE = """SELECT Tasks.Name, Completion_Date.Date FROM Task_Completion
+                                 JOIN Tasks ON Task_ID = Tasks.ID
+                                 JOIN Completion_Date ON Completion_Date_ID = Completion_Date.ID
+                                 WHERE Tasks.Name = (?) AND Completion_Date.Date = (?)"""
+__SQL_GET_TASK_COMPLETION_ID = """SELECT Task_Completion.ID FROM TASK_COMPLETION 
+                                  JOIN Tasks ON Task_ID = Tasks.ID
+                                 JOIN Completion_Date ON Completion_Date_ID = Completion_Date.ID
+                                  WHERE Tasks.Name = (?) AND Completion_Date.Date = (?) """
 
 def connect_to_db() -> tuple[sqlite3.Connection, sqlite3.Cursor]:
-    """Creates the connection to the database, along with the cursor."""
+    """Creates the connection to the database, along with the cursor.Date"""
     connection = sqlite3.connect('MyBooleanLife.db')
     cursor = connection.cursor()
     return connection, cursor
@@ -81,17 +93,44 @@ def add_task(cursor: sqlite3.Cursor, name: str) -> int:
             pass
         else:
             raise e
-    cursor.execute(__SQL_READ_TASK_BY_NAME, (name,))
+    cursor.execute(__SQL_READ_TASK_ID_BY_NAME, (name,))
     task_id = cursor.fetchone()[0]
     return task_id
 
-def log_completion(cursor: sqlite3.Cursor, name: str) -> None:
-    cursor.execute(__SQL_LOG_COMPLETION, (name, datetime.datetime.now().date()))
+def log_completion(cursor: sqlite3.Cursor, name: str) -> int:
+    today = datetime.datetime.now().date()
+    # Check for completion
+    cursor.execute(__SQL_GET_TASK_COMPLETION_ID, (name, today))
+    completion_exists = cursor.fetchone()
+    if not completion_exists:
+        cursor.execute(__SQL_LOG_COMPLETION, (name, today))
+        cursor.execute(__SQL_GET_TASK_COMPLETION_ID, (name, today))
+        result = cursor.fetchone()
+        return result[0]
+    return completion_exists[0]
+
+def read_task_names(cursor: sqlite3.Cursor) -> list[str]:
+    result =cursor.execute(__SQL_READ_TASK_NAMES)
+    # Break out of the listed tuples
+    results = [r[0] for r in result.fetchall()]
+    return results
+
+def get_task_completions(cursor: sqlite3.Cursor, name: str) -> list[tuple[str,str]]:
+    cursor.execute(__SQL_READ_TASK_COMPLETIONS_BY_NAME, (name,))
+    result = cursor.fetchall()
+    return [r[0] for r in result]
+
 
 if __name__ == "__main__":
     connection, cursor = connect_to_db()
     create_tables(cursor)
     initialize_completion_date_table(cursor)
     add_task(cursor, "Weights")
+    add_task(cursor, "Coding")
+    add_task(cursor, "Watch TV")
     log_completion(cursor, "Weights")
+    log_completion(cursor, "Coding")
+    log_completion(cursor, "Watch TV")
+    read_task_names(cursor)
+    get_task_completions(cursor, "Watch TV")
     close_db(connection)
--- MyBooleanLife/main.py
+++ MyBooleanLife/main.py
@@ -1,1 +1,33 @@
-import db
+from . import db
+from urllib.parse import unquote
+# TODO: Initialization process
+# TODO: Need a webserver of sorts. It'll need to communicate with vuejs. Should look into how this will integrate with docker.
+
+from fastapi import FastAPI
+
+app = FastAPI()
+connection, cursor = db.connect_to_db()
+
+@app.get("/")
+async def root():
+    return {"message": "MyBooleanLife"}
+
+@app.get("/alltasks")
+async def all_tasks():
+    """Return all defined tasks"""
+    tasks = db.read_task_names(cursor)
+    return {"message": tasks}
+
+@app.get("/task/{name}")
+async def read_task(name):
+    """Returns all completion dates for task defined in url"""
+    completions = db.get_task_completions(cursor, name)
+    return {"message": completions}
+
+@app.get("/complete/{name}")
+async def complete_task(name):
+    """Logs given task as complete, returning the completion id"""
+    name = unquote(name)
+    print(name)
+    completion_id = db.log_completion(cursor, name)
+    return {"message": completion_id}
